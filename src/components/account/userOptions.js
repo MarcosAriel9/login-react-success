bfcd00979521c8b342a13f2f51adb5860fc06083
MarcosAriel9 <20203tn043@utez.edu.mx>
2022-03-17 14:00:29 -0600

    first commit

--- src/components/account/userOptions.js
+++ src/components/account/userOptions.js
@@ -1,12 +1,68 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import{ListItem} from "react-native-elements"
+import { map } from 'lodash'
 
 export default function userOptions() {
+  const selectComponent=(key)=>{
+switch (key) {
+  case "displayName":
+    
+    break;
+    case "email":
+    
+      break;
+      case "password":
+    
+        break;
+
+  default:
+    break;
+}
+  }
+  const munuOptions=generateOptions()
+  console.log(munuOptions)
   return (
     <View>
-      <Text>OPCIONES DEL USUARIO</Text>
+  <ListItem
+  key={1}
+  title={"cambiar nombre y apellido"}
+  containerStyle={styles.munuItem}
+  onPress={()=>console.log("hoola   ")}
+  />
+   <ListItem
+  key={2}
+  title={"cambiar contraseña"}
+  containerStyle={styles.munuItem}
+  onPress={()=>console.log("contra  ")}
+  />
     </View>
   )
 }
+function generateOptions(){
+return[
+  {    title:"cambiar nombre y apellido",
+  iconType:"material-community",
+  iconName:"account-circle",
+  iconColor:"#ccc"},
+  {
+    title:"cambiar correo electronico",
+    iconType:"material-community",
+    iconName:"at",
+    iconColor:"#ccc"
 
-const styles = StyleSheet.create({})
+},{
+
+  title:"cambiar contraseña",
+  iconType:"material-community",
+  iconName:"lock-reset",
+  iconColor:"#ccc"
+}
+]
+}
+const styles = StyleSheet.create({
+  munuItem:{
+    borderBottomWidth:1,
+    borderBottomColor:"#f3f3f3"
+  }
+})
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -5,12 +5,12 @@ import firebase from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCg9y8zs4vthCz9vR1_j2bGSO0r9DcbFbg",
-  authDomain: "navegacion-7e434.firebaseapp.com",
-  projectId: "navegacion-7e434",
-  storageBucket: "navegacion-7e434.appspot.com",
-  messagingSenderId: "208108662403",
-  appId: "1:208108662403:web:d6eacb1032d45d699db5b6"
+  apiKey: "AIzaSyCZPqI4MmboelDSaEUVKsMVuNpSUd8JBao",
+  authDomain: "reactlogin-7fce1.firebaseapp.com",
+  projectId: "reactlogin-7fce1",
+  storageBucket: "reactlogin-7fce1.appspot.com",
+  messagingSenderId: "779391858300",
+  appId: "1:779391858300:web:89a94ab6554a23241d3bf9"
 };
 
 // Initialize Firebase
